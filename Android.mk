f7c357b215e70424e693eb7434eb58044b2c5f35
Pierre-Hugues Husson <phh@phh.me>
2018-02-24 16:45:32 +0100

    Better deps

--- Android.mk
+++ Android.mk
@@ -7,7 +7,7 @@ vndk-test-sepolicy-sources-27 := $(wildcard $(LOCAL_PATH)/sepolicies/27/*.cil)
 vndk-test-sepolicy-targets-27 := $(patsubst %.cil,%-27-selinux-tested,$(vndk-test-sepolicy-sources-27))
 .PHONY: $(vndk-test-sepolicy-targets-26) $(vndk-test-sepolicy-targets-27)
 
-vndk-test-sepolicy: selinux_policy checkpolicy $(vndk-test-sepolicy-targets-26) $(vndk-test-sepolicy-targets-27)
+vndk-test-sepolicy: $(PRODUCT_OUT)/system/etc/selinux/plat_sepolicy.cil checkpolicy $(vndk-test-sepolicy-targets-26) $(vndk-test-sepolicy-targets-27)
 	echo "Successfully ran:"
 	echo 26: $(vndk-test-sepolicy-sources-26)
 	echo 26: $(vndk-test-sepolicy-targets-26)
@@ -15,9 +15,11 @@ vndk-test-sepolicy: selinux_policy checkpolicy $(vndk-test-sepolicy-targets-26) 
 	echo 27: $(vndk-test-sepolicy-targets-27)
 
 %-26-selinux-tested: %.cil
-	./out/host/linux-x86/bin/secilc $(PRODUCT_OUT)/system/etc/selinux/plat_sepolicy.cil -M true -G -N -c 30 $(PRODUCT_OUT)/system/etc/selinux/mapping/26.0.cil $<
-	./out/host/linux-x86/bin/checkpolicy -M -bC -o /dev/null policy.30
+	./out/host/linux-x86/bin/secilc $(PRODUCT_OUT)/system/etc/selinux/plat_sepolicy.cil -o $@ -M true -G -N -c 30 $(PRODUCT_OUT)/system/etc/selinux/mapping/26.0.cil $<
+	./out/host/linux-x86/bin/checkpolicy -M -bC -o /dev/null $@
+	rm -f $@
 
 %-27-selinux-tested: %.cil
-	./out/host/linux-x86/bin/secilc $(PRODUCT_OUT)/system/etc/selinux/plat_sepolicy.cil -M true -G -N -c 30 $(PRODUCT_OUT)/system/etc/selinux/mapping/27.0.cil $<
-	./out/host/linux-x86/bin/checkpolicy -M -bC -o /dev/null policy.30
+	./out/host/linux-x86/bin/secilc $(PRODUCT_OUT)/system/etc/selinux/plat_sepolicy.cil -o $@ -m -M true -G -N -c 30 $(PRODUCT_OUT)/system/etc/selinux/mapping/27.0.cil $<
+	./out/host/linux-x86/bin/checkpolicy -M -bC -o /dev/null $@
+	rm -f $@
