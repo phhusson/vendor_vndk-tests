46db1dd555aa241c35e6d1fd48326d463167bfa0
Pierre-Hugues Husson <phh@phh.me>
2019-09-14 22:27:41 +0200

    Add some vndk 28 policies, and fix build on AOSP 10

--- Android.mk
+++ Android.mk
@@ -1,25 +1,7 @@
 LOCAL_PATH := $(call my-dir)
 
-vndk-test-sepolicy-sources-26 := $(wildcard $(LOCAL_PATH)/sepolicies/26/*.cil)
-vndk-test-sepolicy-targets-26 := $(patsubst %.cil,%-26-selinux-tested,$(vndk-test-sepolicy-sources-26))
+.PHONY: vndk-test-sepolicy
 
-vndk-test-sepolicy-sources-27 := $(wildcard $(LOCAL_PATH)/sepolicies/27/*.cil)
-vndk-test-sepolicy-targets-27 := $(patsubst %.cil,%-27-selinux-tested,$(vndk-test-sepolicy-sources-27))
-.PHONY: $(vndk-test-sepolicy-targets-26) $(vndk-test-sepolicy-targets-27)
-
-vndk-test-sepolicy: $(PRODUCT_OUT)/system/etc/selinux/plat_sepolicy.cil checkpolicy $(vndk-test-sepolicy-targets-26) $(vndk-test-sepolicy-targets-27)
-	echo "Successfully ran:"
-	echo 26: $(vndk-test-sepolicy-sources-26)
-	echo 26: $(vndk-test-sepolicy-targets-26)
-	echo 27: $(vndk-test-sepolicy-sources-27)
-	echo 27: $(vndk-test-sepolicy-targets-27)
-
-%-26-selinux-tested: %.cil
-	./out/host/linux-x86/bin/secilc $(PRODUCT_OUT)/system/etc/selinux/plat_sepolicy.cil -o $@ -M true -G -N -c 30 $(PRODUCT_OUT)/system/etc/selinux/mapping/26.0.cil $<
-	./out/host/linux-x86/bin/checkpolicy -M -bC -o /dev/null $@
-	rm -f $@
-
-%-27-selinux-tested: %.cil
-	./out/host/linux-x86/bin/secilc $(PRODUCT_OUT)/system/etc/selinux/plat_sepolicy.cil -o $@ -M true -G -N -c 30 $(PRODUCT_OUT)/system/etc/selinux/mapping/27.0.cil $<
-	./out/host/linux-x86/bin/checkpolicy -M -bC -o /dev/null $@
-	rm -f $@
+vndk-test-sepolicy-path := $(LOCAL_PATH)
+vndk-test-sepolicy:
+	bash "$(vndk-test-sepolicy-path)/run.sh" "$(vndk-test-sepolicy-path)" "$(PRODUCT_OUT)"
---
+++ run.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -ex
+
+t="$(mktemp)"
+for vndk in 26 27;do
+	for src in $(find "$1"/sepolicies/$vndk/ -name \*.cil);do
+		./out/host/linux-x86/bin/secilc "$2"/system/etc/selinux/plat_sepolicy.cil -o "$t" -M true -G -N -c 30 "$2"/system/etc/selinux/mapping/${vndk}.0.cil "$src"
+		./out/host/linux-x86/bin/checkpolicy -M -bC -o /dev/null "$t"
+		rm -f "$t"
+	done
+done
+
+for vndk in 28;do
+	for src in $(find "$1"/sepolicies/$vndk -name \*sepolicy.cil);do
+		plat_pub="$(echo "$src" |sed -E 's/_sepolicy.cil$/_plat_pub.cil/g')"
+		./out/host/linux-x86/bin/secilc "$2"/system/etc/selinux/plat_sepolicy.cil -o "$t" -M true -G -N -c 30 "$2"/system/etc/selinux/mapping/${vndk}.0.cil "$plat_pub" "$src"
+		./out/host/linux-x86/bin/checkpolicy -M -bC -o /dev/null "$t"
+		rm -f "$t"
+	done
+done
