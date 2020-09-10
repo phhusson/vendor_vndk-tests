eabcc8e8bd7855e6447f82c1838ed67ba88a8cc4
Pierre-Hugues Husson <phh@phh.me>
2020-09-10 15:52:04 +0200

    Add two new sepolicies

--- run.sh
+++ run.sh
@@ -11,7 +11,7 @@ for vndk in 26 27;do
 	done
 done
 
-for vndk in 28;do
+for vndk in 29 28;do
 	for src in $(find "$1"/sepolicies/$vndk -name \*sepolicy.cil);do
 		plat_pub="$(echo "$src" |sed -E 's/_sepolicy.cil$/_plat_pub.cil/g')"
 		./out/host/linux-x86/bin/secilc "$2"/system/etc/selinux/plat_sepolicy.cil -o "$t" -M true -G -N -c 30 "$2"/system/etc/selinux/mapping/${vndk}.0.cil "$plat_pub" "$src"
