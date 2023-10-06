533390a1d6bc98d86de6b9aab56825c1f03fddcb
Pierre-Hugues Husson <phh@phh.me>
2023-10-06 09:29:41 -0400

    add -m to secilc -- has been added years ago in init

--- run.sh
+++ run.sh
@@ -25,7 +25,7 @@ done
 for vndk in 28 29 30;do
 	for src in $(find "$1"/sepolicies/$vndk -name \*sepolicy.cil);do
 		plat_pub="$(echo "$src" |sed -E 's/_sepolicy.cil$/_plat_pub.cil/g')"
-		./out/host/${TARGET_INCLUDE}-x86/bin/secilc -vvvv "$2"/system/etc/selinux/plat_sepolicy.cil -o "$t" -M true -G -N -c 30 "$2"/system/etc/selinux/mapping/${vndk}.0.cil $sext "$plat_pub" "$src"
+		./out/host/${TARGET_INCLUDE}-x86/bin/secilc -vvvv "$2"/system/etc/selinux/plat_sepolicy.cil -o "$t" -m -M true -G -N -c 30 "$2"/system/etc/selinux/mapping/${vndk}.0.cil $sext "$plat_pub" "$src"
 		./out/host/${TARGET_INCLUDE}-x86/bin/checkpolicy -M -bC -o /dev/null "$t"
 		rm -f "$t"
 	done
